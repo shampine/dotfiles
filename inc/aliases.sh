ef6aa12648c0c636ed4c909705d01aa9e8046b3f
patrickshampine <patrick@patrickshampine.com>
2014-10-25 18:53:55 -0700

    Added a few more aliases

--- inc/aliases.sh
+++ inc/aliases.sh
@@ -1,6 +1,13 @@
 username=$("whoami")
 
+# Permissions
 alias modowner="sudo chown -R $username:_www ."
 alias modfolders="sudo find . -type d -exec chmod g+ws {} \;"
 alias modfiles="sudo find . -type f -exec chmod 664 {} \;"
+
+# Git
+alias gitshit="git fetch --all && git status"
 alias gitmod="git config core.fileMode false && git submodule foreach git config core.filemode false"
+
+# System
+alias lock="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
